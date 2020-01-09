fa8e790b1038da98987d40016086f0d54073d224
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-01-09 21:07:10 +0100

    begin work on topics model and controller

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -3,11 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-const supermarket = require("../models/supermarket.js");
+const topic = require("../models/topics-model.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  supermarket.all(function(data) {
+  topic.all(function(data) {
     const hbsObject = {
       topic: data
     };
--- models/topics-model.js
+++ models/topics-model.js
@@ -1,29 +1,29 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-const cat = {
+const topic = {
   all(cb) {
-    orm.all("cats", function(res) {
+    orm.all("topics", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+    orm.create("topics", cols, vals, function(res) {
       cb(res);
     });
   },
   update(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
+    orm.update("topics", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete(condition, cb) {
-    orm.delete("cats", condition, function(res) {
+    orm.delete("topics", condition, function(res) {
       cb(res);
     });
   }
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = cat;
+module.exports = topic;
