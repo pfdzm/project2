bfd96c43ff4dfbf9c63d828aa52d3463abc99491
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-01-11 14:16:17 +0100

    fix some setup stuff

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/controller.js");
+const forumRoutes = require("./controllers/forum-controller.js");
+// const postsRoutes = require("./controllers/posts-controller.js");
 
-app.use(routes);
+app.use(forumRoutes);
+// app.use(postsRoutes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, () => {
